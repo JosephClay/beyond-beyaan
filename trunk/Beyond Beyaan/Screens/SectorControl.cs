53e9fc6637394959dd635112023463aa32848db9
zeraan55@gmail.com <zeraan55@gmail.com@abb2e6e8-902e-cdb5-a530-f00990ad6eb6>
2013-01-11 20:48:35 +0000

    Added the missing files so I could compile this on other computers.

---
+++ trunk/Beyond Beyaan/Screens/SectorControl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Beyond_Beyaan.Data_Modules;
+
+namespace Beyond_Beyaan.Screens
+{
+	class SectorControl
+	{
+		private Sector sector;
+
+		public SectorControl(Sector sector)
+		{
+			this.sector = sector;
+		}
+
+		public void Draw(DrawingManagement drawingManagement, int x, int y)
+		{
+			switch (sector.SectorType)
+			{
+				case SECTORTYPE.PLANET:
+					DrawPlanetControl(drawingManagement, x, y);
+					break;
+				case SECTORTYPE.EMPTY:
+					DrawEmptyControl(drawingManagement, x, y);
+					break;
+			}
+		}
+
+		private void DrawPlanetControl(DrawingManagement drawingManagement, int x, int y)
+		{
+
+		}
+
+		private void DrawEmptyControl(DrawingManagement drawingManagement, int x, int y)
+		{
+
+		}
+	}
+}
