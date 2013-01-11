4888e1360c13308423ae571d504414213338c531
zeraan55@gmail.com <zeraan55@gmail.com@abb2e6e8-902e-cdb5-a530-f00990ad6eb6>
2013-01-11 20:48:35 +0000

    Added the missing files so I could compile this on other computers.

---
+++ Beyond Beyaan/Data Modules/Sector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Beyond_Beyaan.Data_Modules
+{
+	enum SECTORTYPE { PLANET, DERELICT, EMPTY }
+
+	class Sector
+	{
+		public SECTORTYPE SectorType { get; private set; }
+
+		public Planet Planet { get; private set; }
+
+		public Sector(SECTORTYPE type, Planet planet)
+		{
+			SectorType = type;
+			Planet = planet;
+		}
+	}
+}
