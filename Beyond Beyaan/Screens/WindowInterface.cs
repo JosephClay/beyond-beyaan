b8cefe03ac33fe1fe193382ba4c4da4bdb7a4166
zeraan55@gmail.com <zeraan55@gmail.com@abb2e6e8-902e-cdb5-a530-f00990ad6eb6>
2013-06-10 03:33:16 +0000

    Added TextBox, with a new property, isReadOnly.  There's a slight graphical glitch, need to investigate.  Added files that I forgot to add...

---
+++ Beyond Beyaan/Screens/WindowInterface.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Beyond_Beyaan.Data_Managers;
+
+namespace Beyond_Beyaan.Screens
+{
+	public class WindowInterface
+	{
+		protected GameMain gameMain;
+
+		protected int xPos;
+		protected int yPos;
+		protected int mouseX;
+		protected int mouseY;
+		protected int origX;
+		protected int origY;
+		protected int windowWidth;
+		protected int windowHeight;
+		protected bool moving;
+		protected bool moveable;
+
+		protected BBStretchableImage backGroundImage;
+
+		public bool Initialize(int x, int y, int width, int height, GameMain gameMain, bool moveable, SpriteManager spriteManager, Random r, out string reason)
+		{
+			xPos = x;
+			yPos = y;
+			windowWidth = width;
+			windowHeight = height + 20;
+			this.moveable = moveable;
+
+			moving = false;
+			this.gameMain = gameMain;
+
+			backGroundImage = new BBStretchableImage();
+			if (!backGroundImage.Initialize(x, y, width, height, StretchableImageType.MediumBorder, spriteManager, r, out reason))
+			{
+				return false;
+			}
+			reason = null;
+			return true;
+		}
+
+		public virtual void Draw()
+		{
+			backGroundImage.Draw();
+		}
+
+		public virtual bool MouseHover(int x, int y, float frameDeltaTime)
+		{
+			if (moving)
+			{
+				xPos = (x - mouseX) + origX;
+				yPos = (y - mouseY) + origY;
+
+				if (xPos + windowWidth > gameMain.ScreenWidth)
+				{
+					xPos = gameMain.ScreenWidth - windowWidth;
+				}
+				if (yPos + windowHeight > gameMain.ScreenHeight)
+				{
+					yPos = gameMain.ScreenHeight - windowHeight;
+				}
+				if (xPos < 0)
+				{
+					xPos = 0;
+				}
+				if (yPos < 0)
+				{
+					yPos = 0;
+				}
+				MoveWindow();
+				return true;
+			}
+			if (x >= xPos && x < xPos + windowWidth && y >= yPos && y < yPos + windowHeight)
+			{
+				//Don't want items behind this window to be highlighted
+				return true;
+			}
+			return false;
+		}
+
+		public virtual bool MouseDown(int x, int y)
+		{
+			if (x >= xPos && x < xPos + windowWidth && y >= yPos && y < yPos + windowHeight)
+			{
+				if (moveable)
+				{
+					moving = true;
+					mouseX = x;
+					mouseY = y;
+					origX = xPos;
+					origY = yPos;
+				}
+				return true;
+			}
+			return false;
+		}
+
+		public virtual bool MouseUp(int x, int y)
+		{
+			if (x >= xPos && x < xPos + windowWidth && y >= yPos && y < yPos + windowHeight)
+			{
+				if (moving)
+				{
+					moving = false;
+				}
+
+				return true;
+			}
+			return false;
+		}
+
+		public virtual void MoveWindow()
+		{
+			backGroundImage.MoveTo(xPos, yPos);
+		}
+	}
+}
