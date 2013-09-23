03357e89bd4247743f46b2bf57e3b355910c5902
zeraan55@gmail.com <zeraan55@gmail.com@abb2e6e8-902e-cdb5-a530-f00990ad6eb6>
2013-09-23 17:44:13 +0000

    Finished the research screen.

--- Beyond Beyaan/Screens/ResearchScreen.cs
+++ Beyond Beyaan/Screens/ResearchScreen.cs
@@ -6,33 +6,6 @@ namespace Beyond_Beyaan.Screens
 {
 	public class ResearchScreen : WindowInterface
 	{
-		/*private const int BEAM = 0;
-		private const int PARTICLE = 1;
-		private const int MISSILE = 2;
-		private const int TORPEDO = 3;
-		private const int BOMB = 4;
-		private const int ENGINE = 5;
-		private const int ARMOR = 6;
-		private const int SHIELD = 7;
-		private const int COMPUTER = 8;
-		private const int INFRASTRUCTURE = 9;
-
-		private float researchPoints;
-		private Label researchPointsLabel;
-		private ScrollBar[] techScrollBars;
-		private Button[] researchingTechNames;
-		private Button[] lockedButtons;
-		private Button[] availableTechs;
-		private ProgressBar[] availableTechProgresses;
-		private ScrollBar availableScrollBar;
-		private ProgressBar[] techFieldProgresses;
-
-		private int whichField;
-		private int maxVisible;
-		private int techIndex;*/
-
-		//private BBStretchableImage _background;
-
 		public Action CloseWindow;
 
 		private BBStretchableImage _fieldsBackground;
@@ -206,69 +179,35 @@ namespace Beyond_Beyaan.Screens
 			{
 				result = button.MouseHover(x, y, frameDeltaTime) || result;
 			}
-			return result;
-		}
-
-		/*public void Update(int x, int y, float frameDeltaTime)
-		{
-			for (int i = 0; i < techScrollBars.Length; i++)
+			foreach (var button in _techLockButtons)
 			{
-				if (techScrollBars[i].MouseHover(x, y, frameDeltaTime))
+				result = button.MouseHover(x, y, frameDeltaTime) || result;
+			}
+			for (int i = 0; i < _techSliders.Length; i++)
+			{
+				if (_techSliders[i].MouseHover(x, y, frameDeltaTime))
 				{
+					TechField whichField = TechField.COMPUTER;
 					switch (i)
 					{
-						case BEAM: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.BEAM, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case PARTICLE: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.PARTICLE, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case MISSILE: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.MISSILE, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case TORPEDO: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.TORPEDO, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case BOMB: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.BOMB, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case ENGINE: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.ENGINE, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case ARMOR: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.ARMOR, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case SHIELD: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.SHIELD, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case COMPUTER: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.COMPUTER, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case INFRASTRUCTURE: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.INFRASTRUCTURE, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
+						case 1: whichField = TechField.CONSTRUCTION;
+							break;
+						case 2: whichField = TechField.FORCE_FIELD;
+							break;
+						case 3: whichField = TechField.PLANETOLOGY;
+							break;
+						case 4: whichField = TechField.PROPULSION;
+							break;
+						case 5: whichField = TechField.WEAPON;
+							break;
 					}
+					_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(whichField, _techSliders[i].TopIndex);
+					RefreshSliders();
+					RefreshProgressLabels();
 				}
 			}
-			if (availableScrollBar.MouseHover(x, y, frameDeltaTime))
-			{
-				techIndex = availableScrollBar.TopIndex;
-				RefreshAvailableTechs();
-				return;
-			}
-			for (int i = 0; i < lockedButtons.Length; i++)
-			{
-				lockedButtons[i].MouseHover(x, y, frameDeltaTime);
-			}
-			for (int i = 0; i < researchingTechNames.Length; i++)
-			{
-				researchingTechNames[i].MouseHover(x, y, frameDeltaTime);
-			}
-			for (int i = 0; i < maxVisible; i++)
-			{
-				availableTechs[i].MouseHover(x, y, frameDeltaTime);
-			}
-		}*/
+			return result;
+		}
 
 		public override bool MouseDown(int x, int y)
 		{
@@ -277,26 +216,16 @@ namespace Beyond_Beyaan.Screens
 			{
 				result = button.MouseDown(x, y) || result;
 			}
+			foreach (var slider in _techSliders)
+			{
+				result = slider.MouseDown(x, y) || result;
+			}
+			foreach (var button in _techLockButtons)
+			{
+				result = button.MouseDown(x, y) || result;
+			}
 			return base.MouseDown(x, y) || result;
 		}
-
-		/*public bool MouseDown(int x, int y, int whichButton)
-		{
-			for (int i = 0; i < techScrollBars.Length; i++)
-			{
-				techScrollBars[i].MouseDown(x, y);
-				lockedButtons[i].MouseDown(x, y);
-			}
-			availableScrollBar.MouseDown(x, y);
-			for (int i = 0; i < researchingTechNames.Length; i++)
-			{
-				researchingTechNames[i].MouseDown(x, y);
-			}
-			for (int i = 0; i < maxVisible; i++)
-			{
-				availableTechs[i].MouseDown(x, y);
-			}
-		}*/
 
 		public override bool MouseUp(int x, int y)
 		{
@@ -323,6 +252,57 @@ namespace Beyond_Beyaan.Screens
 					result = true;
 				}
 			}
+			for (int i = 0; i < _techLockButtons.Length; i++)
+			{
+				if (_techLockButtons[i].MouseUp(x, y))
+				{
+					switch (i)
+					{
+						case 0:
+							_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.ComputerLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.ComputerLocked;
+							break;
+						case 1:
+							_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.ConstructionLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.ConstructionLocked;
+							break;
+						case 2:
+							_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.ForceFieldLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.ForceFieldLocked;
+							break;
+						case 3:
+							_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.PlanetologyLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.PlanetologyLocked;
+							break;
+						case 4:
+							_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.PropulsionLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.PropulsionLocked;
+							break;
+						case 5:
+							_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.WeaponLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.WeaponLocked;
+							break;
+					}
+					RefreshLockedStatus();
+				}
+			}
+			for (int i = 0; i < _techSliders.Length; i++)
+			{
+				if (_techSliders[i].MouseUp(x, y))
+				{
+					TechField whichField = TechField.COMPUTER;
+					switch (i)
+					{
+						case 1: whichField = TechField.CONSTRUCTION;
+							break;
+						case 2: whichField = TechField.FORCE_FIELD;
+							break;
+						case 3: whichField = TechField.PLANETOLOGY;
+							break;
+						case 4: whichField = TechField.PROPULSION;
+							break;
+						case 5: whichField = TechField.WEAPON;
+							break;
+					}
+					_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(whichField, _techSliders[i].TopIndex);
+					RefreshSliders();
+					RefreshProgressLabels();
+				}
+			}
 			if (!base.MouseUp(x, y))
 			{
 				//Clicked outside window, close the window
@@ -334,200 +314,6 @@ namespace Beyond_Beyaan.Screens
 			}
 			return result;
 		}
-
-		/*public void MouseUp(int x, int y, int whichButton)
-		{
-			for (int i = 0; i < techScrollBars.Length; i++)
-			{
-				if (techScrollBars[i].MouseUp(x, y))
-				{
-					switch (i)
-					{
-						case BEAM: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.BEAM, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case PARTICLE: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.PARTICLE, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case MISSILE: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.MISSILE, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case TORPEDO: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.TORPEDO, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case BOMB: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.BOMB, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case ENGINE: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.ENGINE, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case ARMOR: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.ARMOR, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case SHIELD: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.SHIELD, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case COMPUTER: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.COMPUTER, techScrollBars[i].TopIndex);
-							SetPercentages(_gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-						case INFRASTRUCTURE: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.SetPercentage(TechField.INFRASTRUCTURE, techScrollBars[i].TopIndex);
-							SetPercentages(gameMain.EmpireManager.CurrentEmpire.TechnologyManager);
-							return;
-					}
-				}
-			}
-			for (int i = 0; i < lockedButtons.Length; i++)
-			{
-				if (lockedButtons[i].MouseUp(x, y))
-				{
-					switch (i)
-					{
-						case BEAM: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.BeamLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.BeamLocked;
-							break;
-						case PARTICLE: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.ParticleLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.ParticleLocked;
-							break;
-						case MISSILE: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.MissileLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.MissileLocked;
-							break;
-						case TORPEDO: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.TorpedoLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.TorpedoLocked;
-							break;
-						case BOMB: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.BombLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.BombLocked;
-							break;
-						case ENGINE: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.EngineLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.EngineLocked;
-							break;
-						case ARMOR: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.ArmorLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.ArmorLocked;
-							break;
-						case SHIELD: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.ShieldLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.ShieldLocked;
-							break;
-						case COMPUTER: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.ComputerLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.ComputerLocked;
-							break;
-						case INFRASTRUCTURE: _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.InfrastructureLocked = !_gameMain.EmpireManager.CurrentEmpire.TechnologyManager.InfrastructureLocked;
-							break;
-					}
-					techScrollBars[i].SetEnabledState(lockedButtons[i].Selected);
-					lockedButtons[i].Selected = !lockedButtons[i].Selected;
-				}
-			}
-			for (int i = 0; i < researchingTechNames.Length; i++)
-			{
-				if (researchingTechNames[i].MouseUp(x, y))
-				{
-					whichField = i;
-					RefreshAvailableTechs();
-					foreach (Button button in researchingTechNames)
-					{
-						button.Selected = false;
-					}
-					researchingTechNames[i].Selected = true;
-				}
-			}
-			if (availableScrollBar.MouseUp(x, y))
-			{
-				techIndex = availableScrollBar.TopIndex;
-				RefreshAvailableTechs();
-				return;
-			}
-			for (int i = 0; i < maxVisible; i++)
-			{
-				if (availableTechs[i].MouseUp(x, y))
-				{
-					TechnologyManager techManager = _gameMain.EmpireManager.CurrentEmpire.TechnologyManager;
-					Technology whichTechToUpdate;
-					switch (whichField)
-					{
-						case BEAM:
-							{
-								techManager.WhichBeamBeingResearched = i + techIndex;
-								whichTechToUpdate = techManager.VisibleBeams[i + techIndex];
-								researchingTechNames[BEAM].SetText(whichTechToUpdate.GetNameWithNextLevel());
-								techFieldProgresses[BEAM].SetMaxProgress(whichTechToUpdate.GetNextLevelCost());
-								techFieldProgresses[BEAM].SetProgress(whichTechToUpdate.GetTotalResearchPoints());
-								techFieldProgresses[BEAM].SetPotentialProgress((int)(researchPoints * (techManager.BeamPercentage * 0.01f)));
-							} break;
-						case PARTICLE:
-							{
-								techManager.WhichParticleBeingResearched = i + techIndex;
-								whichTechToUpdate = techManager.VisibleParticles[i + techIndex];
-								researchingTechNames[PARTICLE].SetText(whichTechToUpdate.GetNameWithNextLevel());
-								techFieldProgresses[PARTICLE].SetMaxProgress(whichTechToUpdate.GetNextLevelCost());
-								techFieldProgresses[PARTICLE].SetProgress(whichTechToUpdate.GetTotalResearchPoints());
-								techFieldProgresses[PARTICLE].SetPotentialProgress((int)(researchPoints * (techManager.ParticlePercentage * 0.01f)));
-							} break;
-						case MISSILE:
-							{
-								techManager.WhichMissileBeingResearched = i + techIndex;
-								whichTechToUpdate = techManager.VisibleMissiles[i + techIndex];
-								researchingTechNames[MISSILE].SetText(whichTechToUpdate.GetNameWithNextLevel());
-								techFieldProgresses[MISSILE].SetMaxProgress(whichTechToUpdate.GetNextLevelCost());
-								techFieldProgresses[MISSILE].SetProgress(whichTechToUpdate.GetTotalResearchPoints());
-								techFieldProgresses[MISSILE].SetPotentialProgress((int)(researchPoints * (techManager.MissilePercentage * 0.01f)));
-							} break;
-						case TORPEDO:
-							{
-								techManager.WhichTorpedoBeingResearched = i + techIndex;
-								whichTechToUpdate = techManager.VisibleTorpedoes[i + techIndex];
-								researchingTechNames[TORPEDO].SetText(whichTechToUpdate.GetNameWithNextLevel());
-								techFieldProgresses[TORPEDO].SetMaxProgress(whichTechToUpdate.GetNextLevelCost());
-								techFieldProgresses[TORPEDO].SetProgress(whichTechToUpdate.GetTotalResearchPoints());
-								techFieldProgresses[TORPEDO].SetPotentialProgress((int)(researchPoints * (techManager.TorpedoPercentage * 0.01f)));
-							} break;
-						case BOMB:
-							{
-								techManager.WhichBombBeingResearched = i + techIndex;
-								whichTechToUpdate = techManager.VisibleBombs[i + techIndex];
-								researchingTechNames[BOMB].SetText(whichTechToUpdate.GetNameWithNextLevel());
-								techFieldProgresses[BOMB].SetMaxProgress(whichTechToUpdate.GetNextLevelCost());
-								techFieldProgresses[BOMB].SetProgress(whichTechToUpdate.GetTotalResearchPoints());
-								techFieldProgresses[BOMB].SetPotentialProgress((int)(researchPoints * (techManager.BombPercentage * 0.01f)));
-							} break;
-						case ENGINE:
-							{
-								techManager.WhichEngineBeingResearched = i + techIndex;
-								whichTechToUpdate = techManager.VisibleEngines[i + techIndex];
-								researchingTechNames[ENGINE].SetText(whichTechToUpdate.GetNameWithNextLevel());
-								techFieldProgresses[ENGINE].SetMaxProgress(whichTechToUpdate.GetNextLevelCost());
-								techFieldProgresses[ENGINE].SetProgress(whichTechToUpdate.GetTotalResearchPoints());
-								techFieldProgresses[ENGINE].SetPotentialProgress((int)(researchPoints * (techManager.EnginePercentage * 0.01f)));
-							} break;
-						case ARMOR:
-							{
-								techManager.WhichArmorBeingResearched = i + techIndex;
-								whichTechToUpdate = techManager.VisibleArmors[i + techIndex];
-								researchingTechNames[ARMOR].SetText(whichTechToUpdate.GetNameWithNextLevel());
-								techFieldProgresses[ARMOR].SetMaxProgress(whichTechToUpdate.GetNextLevelCost());
-								techFieldProgresses[ARMOR].SetProgress(whichTechToUpdate.GetTotalResearchPoints());
-								techFieldProgresses[ARMOR].SetPotentialProgress((int)(researchPoints * (techManager.ArmorPercentage * 0.01f)));
-							} break;
-						case SHIELD:
-							{
-								techManager.WhichShieldBeingResearched = i + techIndex;
-								whichTechToUpdate = techManager.VisibleShields[i + techIndex];
-								researchingTechNames[SHIELD].SetText(whichTechToUpdate.GetNameWithNextLevel());
-								techFieldProgresses[SHIELD].SetMaxProgress(whichTechToUpdate.GetNextLevelCost());
-								techFieldProgresses[SHIELD].SetProgress(whichTechToUpdate.GetTotalResearchPoints());
-								techFieldProgresses[SHIELD].SetPotentialProgress((int)(researchPoints * (techManager.ShieldPercentage * 0.01f)));
-							} break;
-						case COMPUTER:
-							{
-								techManager.WhichComputerBeingResearched = i + techIndex;
-								whichTechToUpdate = techManager.VisibleComputers[i + techIndex];
-								researchingTechNames[COMPUTER].SetText(whichTechToUpdate.GetNameWithNextLevel());
-								techFieldProgresses[COMPUTER].SetMaxProgress(whichTechToUpdate.GetNextLevelCost());
-								techFieldProgresses[COMPUTER].SetProgress(whichTechToUpdate.GetTotalResearchPoints());
-								techFieldProgresses[COMPUTER].SetPotentialProgress((int)(researchPoints * (techManager.ComputerPercentage * 0.01f)));
-							} break;
-						case INFRASTRUCTURE:
-							{
-								techManager.WhichInfrastructureBeingResearched = i + techIndex;
-								whichTechToUpdate = techManager.VisibleInfrastructures[i + techIndex];
-								researchingTechNames[INFRASTRUCTURE].SetText(whichTechToUpdate.GetNameWithNextLevel());
-								techFieldProgresses[INFRASTRUCTURE].SetMaxProgress(whichTechToUpdate.GetNextLevelCost());
-								techFieldProgresses[INFRASTRUCTURE].SetProgress(whichTechToUpdate.GetTotalResearchPoints());
-								techFieldProgresses[INFRASTRUCTURE].SetPotentialProgress((int)(researchPoints * (techManager.InfrastructurePercentage * 0.01f)));
-							} break;
-					}
-				}
-			}
-		}*/
 
 		public void MouseScroll(int direction, int x, int y)
 		{
