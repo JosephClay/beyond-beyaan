dfb9fff8ba299f1fe7a9847127ff96f518c1555b
zeraan55@gmail.com <zeraan55@gmail.com@abb2e6e8-902e-cdb5-a530-f00990ad6eb6>
2013-10-01 16:52:34 +0000

    Defenses and Computer components added to ship design screen.

--- trunk/Beyond Beyaan/Screens/ShipDesignScreen.cs
+++ trunk/Beyond Beyaan/Screens/ShipDesignScreen.cs
@@ -16,13 +16,23 @@ namespace Beyond_Beyaan.Screens
 		private BBStretchableImage _engineBackground;
 		private BBStretchButton _engineButton;
 		private BBStretchButton _maneuverButton;
-		private BBLabel _engineLabel;
-		private BBLabel _maneuverLabel;
 		private BBLabel _engineSpeed;
 		private BBLabel _combatSpeed;
 		private BBLabel _costPerPower;
 		private BBLabel _spacePerPower;
 		private BBLabel _defenseRating;
+
+		private BBStretchableImage _defensiveEquipmentBackground;
+		private BBStretchButton _armorButton;
+		private BBStretchButton _shieldButton;
+		private BBStretchButton _ECMButton;
+		private BBLabel _hitPointsLabel;
+		private BBLabel _absorbtionLabel;
+		private BBLabel _missileDefenseLabel;
+
+		private BBStretchableImage _computerBackground;
+		private BBStretchButton _computerButton;
+		private BBLabel _attackRating;
 
 		private BBSprite _shipSprite;
 
@@ -79,53 +89,94 @@ namespace Beyond_Beyaan.Screens
 			_engineBackground = new BBStretchableImage();
 			_engineButton = new BBStretchButton();
 			_maneuverButton = new BBStretchButton();
-			_engineLabel = new BBLabel();
-			_maneuverLabel = new BBLabel();
 			_engineSpeed = new BBLabel();
 			_combatSpeed = new BBLabel();
 			_costPerPower = new BBLabel();
 			_spacePerPower = new BBLabel();
 			_defenseRating = new BBLabel();
 
-			if (!_engineBackground.Initialize(x + 15, y + 15, 300, 185, StretchableImageType.ThinBorderBG, gameMain.Random, out reason))
+			if (!_engineBackground.Initialize(x + 15, y + 15, 300, 180, StretchableImageType.ThinBorderBG, gameMain.Random, out reason))
 			{
 				return false;
 			}
-			if (!_engineButton.Initialize(string.Empty, ButtonTextAlignment.CENTER, StretchableImageType.TinyButtonBG, StretchableImageType.TinyButtonFG, x + 135, y + 25, 170, 35, gameMain.Random, out reason))
+			if (!_engineButton.Initialize(string.Empty, ButtonTextAlignment.CENTER, StretchableImageType.TinyButtonBG, StretchableImageType.TinyButtonFG, x + 25, y + 25, 280, 35, gameMain.Random, out reason))
 			{
 				return false;
 			}
-			if (!_maneuverButton.Initialize(string.Empty, ButtonTextAlignment.CENTER, StretchableImageType.TinyButtonBG, StretchableImageType.TinyButtonFG, x + 135, y + 67, 170, 35, gameMain.Random, out reason))
+			if (!_maneuverButton.Initialize(string.Empty, ButtonTextAlignment.CENTER, StretchableImageType.TinyButtonBG, StretchableImageType.TinyButtonFG, x + 25, y + 62, 280, 35, gameMain.Random, out reason))
 			{
 				return false;
 			}
-			if (!_engineLabel.Initialize(x + 130, y + 30, "Engine: ", System.Drawing.Color.White, out reason))
+			if (!_engineSpeed.Initialize(x + 25, y + 100, string.Empty, System.Drawing.Color.White, out reason))
 			{
 				return false;
 			}
-			if (!_maneuverLabel.Initialize(x + 130, y + 72, "Maneuver: ", System.Drawing.Color.White, out reason))
+			if (!_combatSpeed.Initialize(x + 165, y + 100, string.Empty, System.Drawing.Color.White, out reason))
 			{
 				return false;
 			}
-			_engineLabel.SetAlignment(true);
-			_maneuverLabel.SetAlignment(true);
-			if (!_engineSpeed.Initialize(x + 25, y + 105, string.Empty, System.Drawing.Color.White, out reason))
+			if (!_costPerPower.Initialize(x + 25, y + 120, string.Empty, System.Drawing.Color.White, out reason))
 			{
 				return false;
 			}
-			if (!_combatSpeed.Initialize(x + 165, y + 105, string.Empty, System.Drawing.Color.White, out reason))
+			if (!_spacePerPower.Initialize(x + 25, y + 140, string.Empty, System.Drawing.Color.White, out reason))
 			{
 				return false;
 			}
-			if (!_costPerPower.Initialize(x + 25, y + 125, string.Empty, System.Drawing.Color.White, out reason))
+			if (!_defenseRating.Initialize(x + 25, y + 160, string.Empty, System.Drawing.Color.White, out reason))
 			{
 				return false;
 			}
-			if (!_spacePerPower.Initialize(x + 25, y + 145, string.Empty, System.Drawing.Color.White, out reason))
+
+			_defensiveEquipmentBackground = new BBStretchableImage();
+			_armorButton = new BBStretchButton();
+			_shieldButton = new BBStretchButton();
+			_ECMButton = new BBStretchButton();
+			_hitPointsLabel = new BBLabel();
+			_absorbtionLabel = new BBLabel();
+			_missileDefenseLabel = new BBLabel();
+			if (!_defensiveEquipmentBackground.Initialize(x + 15, y + 195, 300, 190, StretchableImageType.ThinBorderBG, gameMain.Random, out reason))
 			{
 				return false;
 			}
-			if (!_defenseRating.Initialize(x + 25, y + 165, string.Empty, System.Drawing.Color.White, out reason))
+			if (!_armorButton.Initialize(string.Empty, ButtonTextAlignment.CENTER, StretchableImageType.TinyButtonBG, StretchableImageType.TinyButtonFG, x + 25, y + 206, 280, 35, gameMain.Random, out reason))
+			{
+				return false;
+			}
+			if (!_shieldButton.Initialize(string.Empty, ButtonTextAlignment.CENTER, StretchableImageType.TinyButtonBG, StretchableImageType.TinyButtonFG, x + 25, y + 243, 280, 35, gameMain.Random, out reason))
+			{
+				return false;
+			}
+			if (!_ECMButton.Initialize(string.Empty, ButtonTextAlignment.CENTER, StretchableImageType.TinyButtonBG, StretchableImageType.TinyButtonFG, x + 25, y + 280, 280, 35, gameMain.Random, out reason))
+			{
+				return false;
+			}
+			if (!_hitPointsLabel.Initialize(x + 25, y + 315, string.Empty, System.Drawing.Color.White, out reason))
+			{
+				return false;
+			}
+			if (!_absorbtionLabel.Initialize(x + 25, y + 335, string.Empty, System.Drawing.Color.White, out reason))
+			{
+				return false;
+			}
+			if (!_missileDefenseLabel.Initialize(x + 25, y + 355, string.Empty, System.Drawing.Color.White, out reason))
+			{
+				return false;
+			}
+
+			_computerBackground = new BBStretchableImage();
+			_computerButton = new BBStretchButton();
+			_attackRating = new BBLabel();
+
+			if (!_computerBackground.Initialize(x + 315, y + 15, 470, 55, StretchableImageType.ThinBorderBG, gameMain.Random, out reason))
+			{
+				return false;
+			}
+			if (!_computerButton.Initialize(string.Empty, ButtonTextAlignment.CENTER, StretchableImageType.TinyButtonBG, StretchableImageType.TinyButtonFG, x + 325, y + 25, 280, 35, gameMain.Random, out reason))
+			{
+				return false;
+			}
+			if (!_attackRating.Initialize(x + 610, y + 30, string.Empty, System.Drawing.Color.White, out reason))
 			{
 				return false;
 			}
@@ -148,8 +199,6 @@ namespace Beyond_Beyaan.Screens
 			_shipSprite.Draw(_xPos + 125, _yPos + 485);
 			GorgonLibrary.Gorgon.CurrentShader = null;
 			_engineBackground.Draw();
-			_engineLabel.Draw();
-			_maneuverLabel.Draw();
 			_engineButton.Draw();
 			_maneuverButton.Draw();
 			_engineSpeed.Draw();
@@ -157,6 +206,16 @@ namespace Beyond_Beyaan.Screens
 			_costPerPower.Draw();
 			_spacePerPower.Draw();
 			_defenseRating.Draw();
+			_defensiveEquipmentBackground.Draw();
+			_armorButton.Draw();
+			_shieldButton.Draw();
+			_ECMButton.Draw();
+			_hitPointsLabel.Draw();
+			_absorbtionLabel.Draw();
+			_missileDefenseLabel.Draw();
+			_computerBackground.Draw();
+			_computerButton.Draw();
+			_attackRating.Draw();
 		}
 
 		public override bool MouseHover(int x, int y, float frameDeltaTime)
@@ -191,7 +250,7 @@ namespace Beyond_Beyaan.Screens
 				if (_shipSizeButtons[i].MouseUp(x, y))
 				{
 					_shipDesign.Size = i;
-					RefreshShipSprite();
+					RefreshAll();
 					return true;
 				}
 			}
@@ -230,8 +289,18 @@ namespace Beyond_Beyaan.Screens
 		{
 			_shipDesign = _gameMain.EmpireManager.CurrentEmpire.FleetManager.LastShipDesign;
 			_techLevels = _gameMain.EmpireManager.CurrentEmpire.TechnologyManager.GetFieldLevels();
+			RefreshAll();
+		}
+
+		private void RefreshAll()
+		{
+			//Used when changing ship size or initial loading of ship design
 			RefreshShipSprite();
 			RefreshEngineLabels();
+			RefreshShield();
+			RefreshHP();
+			RefreshECM();
+			RefreshComputer();
 		}
 
 		private void RefreshShipSprite()
@@ -249,12 +318,60 @@ namespace Beyond_Beyaan.Screens
 		private void RefreshEngineLabels()
 		{
 			_engineButton.SetText(_shipDesign.Engine.Key.DisplayName);
-			_maneuverButton.SetText(_shipDesign.ManeuverSpeed.ToString());
+			_maneuverButton.SetText(string.Format("{0} Maneuverability", _shipDesign.ManeuverSpeed.ToString()));
 			_engineSpeed.SetText(string.Format("Galaxy Speed: {0}", _shipDesign.GalaxySpeed));
 			_combatSpeed.SetText(string.Format("Combat Speed: {0}", (_shipDesign.ManeuverSpeed / 2) + 1));
 			_costPerPower.SetText(string.Format("Cost per Power: {0:0.0} BCs", _shipDesign.Engine.Key.GetCost(_techLevels, _shipDesign.Size) / (_shipDesign.GalaxySpeed * 10)));
 			_spacePerPower.SetText(string.Format("Space per Power: {0:0.0} DWT", _shipDesign.Engine.Key.GetSize(_techLevels, _shipDesign.Size) / (_shipDesign.GalaxySpeed * 10)));
-			_defenseRating.SetText(string.Format("Defense Rating: {0}", _shipDesign.DefenseRating));
+			_defenseRating.SetText(string.Format("{0} Defense Rating", _shipDesign.DefenseRating));
+		}
+
+		private void RefreshHP()
+		{
+			_armorButton.SetText(_shipDesign.Armor.DisplayName);
+			_hitPointsLabel.SetText(string.Format("{0} Hit Points", _shipDesign.MaxHitPoints));
+		}
+
+		private void RefreshShield()
+		{
+			if (_shipDesign.Shield != null)
+			{
+				_shieldButton.SetText(_shipDesign.Shield.DisplayName);
+				_absorbtionLabel.SetText(string.Format("Absorbs {0} damage", _shipDesign.Shield.Technology.Shield));
+			}
+			else
+			{
+				_shieldButton.SetText("No Shield");
+				_absorbtionLabel.SetText("Absorbs 0 damage");
+			}
+		}
+
+		private void RefreshECM()
+		{
+			if (_shipDesign.ECM != null)
+			{
+				_ECMButton.SetText(_shipDesign.ECM.DisplayName);
+				_missileDefenseLabel.SetText(string.Format("{0} Missile Defense Rating", _shipDesign.ECM.Technology.ECM));
+			}
+			else
+			{
+				_ECMButton.SetText("No ECM");
+				_missileDefenseLabel.SetText("0 Missile Defense Rating");
+			}
+		}
+
+		private void RefreshComputer()
+		{
+			if (_shipDesign.Computer != null)
+			{
+				_computerButton.SetText(_shipDesign.Computer.DisplayName);
+				_attackRating.SetText(string.Format("{0} Attack Rating", _shipDesign.Computer.Technology.BattleComputer));
+			}
+			else
+			{
+				_computerButton.SetText("No Computer");
+				_attackRating.SetText("0 Attack Rating");
+			}
 		}
 	}
 }
